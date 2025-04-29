c3081eb7741a582b7164b96f8a73c879aa7d759a
Трофим Пизик <tpizik@MacBook-Pro-Trofim.local>
2025-04-29 21:19:50 +0300

    iter5; Added env

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
@@ -15,6 +16,13 @@ func ParseConfig() Config {
 	flag.StringVar(&flagRunAddr, "a", "127.0.0.1:8080", "address and port to run server")
 	flag.StringVar(&flagShortAddr, "b", "http://127.0.0.1:8080", "base address of the resulting shorthand url")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		flagShortAddr = envBaseURL
+	}
 
 	newConfig := Config{
 		RunAddr:   flagRunAddr,
